aec5c6ed8f91f1efb7a36bf1b69c00dedc598b60
dimka77783 <dimka77783@mai.ru>
2022-07-24 19:11:07 +0300

    29

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import ListView
+from django.views.generic import ListView, DeleteView
 from .models import News, Category
 from .forms import NewsForm
 
@@ -31,6 +31,11 @@ class NewsByCategory(ListView):
     def get_queryset(self):
         return News.objects.filter(category_id=self.kwargs['category_id'], is_published=True)
 
+class ViewNews(DeleteView):
+    model = News
+    #pk_url_kwarg = 'news_id'
+    #template_name = 'news/news_confirm_delete.html'
+    context_object_name = 'news_item'
 
 #def index(request):
     #news = News.objects.all()
@@ -40,15 +45,15 @@ class NewsByCategory(ListView):
     #}
     #return render(request, 'news/index.html', context=context)
 
-def get_category(request, category_id):
-    news = News.objects.filter(category_id=category_id)
-    category = Category.objects.get(pk=category_id)
-    return render(request, 'news/category.html', { 'news':news, 'category': category})
+#def get_category(request, category_id):
+    #news = News.objects.filter(category_id=category_id)
+    #category = Category.objects.get(pk=category_id)
+    #return render(request, 'news/category.html', { 'news':news, 'category': category})
 
-def view_news(request, news_id):
+#def view_news(request, news_id):
     #news_item = News.objects.get(pk=news_id)
-    news_item = get_object_or_404(News, pk=news_id)
-    return render(request, 'news/view_news.html', {"news_item": news_item})
+    #news_item = get_object_or_404(News, pk=news_id)
+    #return render(request, 'news/view_news.html', {"news_item": news_item})
 
 def add_news(request):
     if request.method == 'POST':

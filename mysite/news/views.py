4be74370588c0e3ba9e2720c1148d3d8842e2796
dimka77783 <dimka77783@mai.ru>
2022-07-25 20:40:30 +0300

    30

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import ListView, DeleteView
+from django.views.generic import ListView, DeleteView, CreateView
 from .models import News, Category
 from .forms import NewsForm
+from django.urls import reverse_lazy
 
 class HomeNews(ListView):
     model = News
@@ -37,6 +38,12 @@ class ViewNews(DeleteView):
     #template_name = 'news/news_confirm_delete.html'
     context_object_name = 'news_item'
 
+
+class CreateNews(CreateView):
+    form_class = NewsForm
+    template_name = 'news/add_news.html'
+    #success_url = reverse_lazy('home')
+
 #def index(request):
     #news = News.objects.all()
     #context = {
@@ -55,17 +62,17 @@ class ViewNews(DeleteView):
     #news_item = get_object_or_404(News, pk=news_id)
     #return render(request, 'news/view_news.html', {"news_item": news_item})
 
-def add_news(request):
-    if request.method == 'POST':
-        form = NewsForm(request.POST)
-        if form.is_valid():
+#def add_news(request):
+    #if request.method == 'POST':
+        #form = NewsForm(request.POST)
+        #if form.is_valid():
             #  print(form.cleaned_data)
             # news = News.objects.create(**form.cleaned_data)
-            news = form.save()
-            return redirect(news)
+            #news = form.save()
+            #return redirect(news)
 
-    else:
-        form = NewsForm()
-    return render(request, 'news/add_news.html', {'form': form})
+    #else:
+        #form = NewsForm()
+    #return render(request, 'news/add_news.html', {'form': form})
 
 

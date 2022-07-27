aa72c8e6aa2ea7050e744fc64fb7ea1d19516608
dimka77783 <dimka77783@mai.ru>
2022-07-27 20:36:31 +0300

    37

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.urls import reverse
 
+
 class News(models.Model):
     title = models.CharField(max_length=150, verbose_name= 'Наименование')
     content = models.TextField(blank=True, verbose_name= 'Контент')
@@ -9,6 +10,7 @@ class News(models.Model):
     photo = models.ImageField(upload_to='photos/%Y/%m/%d/', verbose_name= 'Фото', blank=True)
     is_published = models.BooleanField(default=True, verbose_name= 'Опубликовано')
     category = models.ForeignKey('Category', on_delete=models.PROTECT,  verbose_name= 'Категория')
+    views = models.IntegerField(default=0)
 
     def get_absolute_url(self):
         return reverse('view_news', kwargs={"pk": self.pk})

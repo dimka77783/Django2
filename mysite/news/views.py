24444d058ec9531c86e69356d76723f6ba238c2d
dimka77783 <dimka77783@mai.ru>
2022-08-10 19:49:41 +0300

    46

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,11 +1,32 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import ListView, DeleteView, CreateView
-from django.contrib.auth.mixins import LoginRequiredMixin
-from .models import News, Category
-from .forms import NewsForm
-from .utils import MyMixin
+from django.views.generic import ListView, DetailView, CreateView
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.paginator import Paginator
+from .models import News, Category
+from .forms import NewsForm, UserRegisterForm
+from .utils import MyMixin
+from django.contrib import messages
+
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Вы успешно зарегистрировались')
+            return redirect('login')
+        else:
+            messages.error(request, 'Ошибка регистрации')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'news/register.html', {"form": form})
+
+
+def login(request):
+    return render(request, 'news/login.html')
+
 
 def test(request):
     objects = ['john1', 'paul2', 'george3', 'ringo4', 'john5', 'paul6', 'george7']
@@ -19,12 +40,11 @@ class HomeNews(MyMixin, ListView):
     model = News
     template_name = 'news/home_news_list.html'
     context_object_name = 'news'
-    extra_context = {'title': 'Главная'}
     mixin_prop = 'hello world'
     paginate_by = 2
 
     def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(** kwargs)
+        context = super().get_context_data(**kwargs)
         context['title'] = self.get_upper('Главная страница')
         context['mixin_prop'] = self.get_prop()
         return context
@@ -41,7 +61,7 @@ class NewsByCategory(MyMixin, ListView):
     paginate_by = 2
 
     def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(** kwargs)
+        context = super().get_context_data(**kwargs)
         context['title'] = self.get_upper(Category.objects.get(pk=self.kwargs['category_id']))
         return context
 
@@ -49,11 +69,11 @@ class NewsByCategory(MyMixin, ListView):
         return News.objects.filter(category_id=self.kwargs['category_id'], is_published=True).select_related('category')
 
 
-class ViewNews(DeleteView):
+class ViewNews(DetailView):
     model = News
-    #pk_url_kwarg = 'news_id'
-    #template_name = 'news/news_confirm_delete.html'
     context_object_name = 'news_item'
+    # template_name = 'news/news_detail.html'
+    # pk_url_kwarg = 'news_id'
 
 
 class CreateNews(LoginRequiredMixin, CreateView):
@@ -62,35 +82,37 @@ class CreateNews(LoginRequiredMixin, CreateView):
     # success_url = reverse_lazy('home')
     # login_url = '/admin/'
     raise_exception = True
-#def index(request):
-    #news = News.objects.all()
-    #context = {
-        #'news': news,
-        #'title': 'Список новостей',
-    #}
-    #return render(request, 'news/index.html', context=context)
-
-#def get_category(request, category_id):
-    #news = News.objects.filter(category_id=category_id)
-    #category = Category.objects.get(pk=category_id)
-    #return render(request, 'news/category.html', { 'news':news, 'category': category})
-
-#def view_news(request, news_id):
-    #news_item = News.objects.get(pk=news_id)
-    #news_item = get_object_or_404(News, pk=news_id)
-    #return render(request, 'news/view_news.html', {"news_item": news_item})
-
-#def add_news(request):
-    #if request.method == 'POST':
-        #form = NewsForm(request.POST)
-        #if form.is_valid():
-            #  print(form.cleaned_data)
-            # news = News.objects.create(**form.cleaned_data)
-            #news = form.save()
-            #return redirect(news)
-
-    #else:
-        #form = NewsForm()
-    #return render(request, 'news/add_news.html', {'form': form})
 
 
+# def index(request):
+#     news = News.objects.all()
+#     context = {
+#         'news': news,
+#         'title': 'Список новостей',
+#     }
+#     return render(request, template_name='news/index.html', context=context)
+
+
+# def get_category(request, category_id):
+#     news = News.objects.filter(category_id=category_id)
+#     category = Category.objects.get(pk=category_id)
+#     return render(request, 'news/category.html', {'news': news, 'category': category})
+
+
+# def view_news(request, news_id):
+#     # news_item = News.objects.get(pk=news_id)
+#     news_item = get_object_or_404(News, pk=news_id)
+#     return render(request, 'news/view_news.html', {"news_item": news_item})
+
+
+# def add_news(request):
+#     if request.method == 'POST':
+#         form = NewsForm(request.POST)
+#         if form.is_valid():
+#             # print(form.cleaned_data)
+#             # news = News.objects.create(**form.cleaned_data)
+#             news = form.save()
+#             return redirect(news)
+#     else:
+#         form = NewsForm()
+#     return render(request, 'news/add_news.html', {'form': form})

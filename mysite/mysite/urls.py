e007da6029e837aba4873ed15748d2144f561b6f
dimka77783 <dimka77783@mai.ru>
2022-08-19 19:24:43 +0300

    50

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -21,6 +21,8 @@ from news.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('captcha/', include('captcha.urls')),
     path('', include('news.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:

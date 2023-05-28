ceaccc547fa439280e31b4a1f12d095acea0dac8
odinokov <dimka77783@mail.ru>
2023-05-28 21:09:59 +0300

    50.2

--- mysite/testapp/admin.py
+++ mysite/testapp/admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
-from .models import Rubric
+#from mptt.admin import MPTTModelAdmin
+from mptt.admin import DraggableMPTTAdmin
+from .models import Rubric, Article
 
-admin.site.register(Rubric)
+
+#admin.site.register(Rubric, MPTTModelAdmin)
+admin.site.register(Article)
+admin.site.register(
+    Rubric,
+    DraggableMPTTAdmin,
+    list_display=(
+        'tree_actions',
+        'indented_title',
+        # ...more fields if you feel like it...
+    ),
+    list_display_links=(
+        'indented_title',
+    ),
+)
